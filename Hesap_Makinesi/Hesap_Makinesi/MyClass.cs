444251aed81351938916790ecd309060e9f74299
Murat <32733938+MuratTuz@users.noreply.github.com>
2018-03-16 11:44:59 +0300

    Add files via upload
    some codes are added

--- Hesap_Makinesi/Hesap_Makinesi/MyClass.cs
+++ Hesap_Makinesi/Hesap_Makinesi/MyClass.cs
@@ -13,6 +13,35 @@ namespace Hesap_Makinesi
      */
     public static class MyStringClass
     {
+        /*
+        var count=listOfStrings.Count();
+        var result=listOfStrings.Aggregate(
+           new { i=0, v=String.Empty},
+           (x,y) => new { i=x.i+1, v=(x.i+1 == count) ? x.v + "&" + y : x.v+","+y},
+           x=>x.v); 
+
+            int i = 0;
+            string strResult = listOfStrings.Where(s => ++i != listOfStrings.Count)
+                                .Aggregate("", (current, s) => current + s);
+
+
+            int count = 0;
+            var aggregated = items.Aggregate(new StringBuilder(), (acc, item) =>
+                            {
+                                if (++count == items.Count)
+                                {
+                                    // it's the last item
+                                    acc.Append("last");
+                                }
+                                else
+                                {
+                                    acc.Append("not last, ");
+                                }
+                                return acc;
+                            }).ToString();
+        */
+
+
         public static string ZeroIfEmpty(this string s)
         {
             return string.IsNullOrEmpty(s) ? "0" : s;
